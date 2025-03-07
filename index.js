da456934dfff9a9822ba76be422851196e11d215
LYDIA <lydiakinuthia91@gmail.com>
2025-03-07 12:29:16 +0300

    functions

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Examples of usage:
+  console.log(saturdayFun());
+
+  function mondayWork(task = "go to the office") {
+    return `This Monday, I will ${task}.`;
+  }
+  
+  // Examples of usage:
+  console.log(mondayWork());
+
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  const simplePromptFunction = wrapAdjective("~");
+console.log(simplePromptFunction("a star"));
